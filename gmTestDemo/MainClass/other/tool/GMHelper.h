22eb2586204d0ae97e49a6668139cc757a4d1405
gengman <397005462@qq.com>
2018-04-28 10:54:37 +0800

    优化代码

---
+++ gmTestDemo/MainClass/other/tool/GMHelper.h
@@ -0,0 +1,13 @@
+//
+//  GMHelper.h
+//  gmTestDemo
+//
+//  Created by lemonmgy on 2018/4/27.
+//  Copyright © 2018年 lemonmgy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GMHelper : NSObject
+
+@end
---
+++ gmTestDemo/MainClass/other/tool/GMHelper.m
@@ -0,0 +1,13 @@
+//
+//  GMHelper.m
+//  gmTestDemo
+//
+//  Created by lemonmgy on 2018/4/27.
+//  Copyright © 2018年 lemonmgy. All rights reserved.
+//
+
+#import "GMHelper.h"
+
+@implementation GMHelper
+
+@end
